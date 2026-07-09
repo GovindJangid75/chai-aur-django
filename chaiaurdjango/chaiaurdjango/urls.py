a4bec4688c1804d66ca796e91efa2673e43bf0ed
GovindJangid75 <govindjangidt@gmail.com>
2026-07-09 21:28:30 +0530

    url file changes

--- chaiaurdjango/chaiaurdjango/urls.py
+++ chaiaurdjango/chaiaurdjango/urls.py
@@ -21,6 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('contac', views.contact, name="contact"),
+    path('contact', views.contact, name="contactpage"),
 
 ]
